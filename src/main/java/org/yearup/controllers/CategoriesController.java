720bc9722bf486262da7a1d0700218968daec921
samatisz <shelbymatisz@gmail.com>
2024-06-24 13:07:09 -0400

    post lunch push

--- src/main/java/org/yearup/controllers/CategoriesController.java
+++ src/main/java/org/yearup/controllers/CategoriesController.java
@@ -10,9 +10,8 @@ import org.yearup.models.Product;
 
 import java.util.List;
 
-// add the annotations to make this a REST controller
-// add the annotation to make this controller the endpoint for the following url
-    // http://localhost:8080/categories
+//TODO add the annotation to make this controller the endpoint for the following url
+// http://localhost:8080/categories
 // add annotation to allow cross site origin requests
 
 @RestController
--- src/main/java/org/yearup/controllers/ShoppingCartController.java
+++ src/main/java/org/yearup/controllers/ShoppingCartController.java
@@ -1,6 +1,10 @@
 package org.yearup.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 import org.yearup.data.ProductDao;
 import org.yearup.data.ShoppingCartDao;
@@ -10,16 +14,19 @@ import org.yearup.models.User;
 
 import java.security.Principal;
 
-// convert this class to a REST controller
 // only logged in users should have access to these actions
-public class ShoppingCartController
-{
-    // a shopping cart requires
-    private ShoppingCartDao shoppingCartDao;
-    private UserDao userDao;
-    private ProductDao productDao;
+@RestController
+public class ShoppingCartController { // a shopping cart requires
+    private final ShoppingCartDao shoppingCartDao;
+    private final UserDao userDao;
+    private final ProductDao productDao;
 
-
+    @Autowired
+    public ShoppingCartController(ShoppingCartDao shoppingCartDao, UserDao userDao, ProductDao productDao) {
+        this.shoppingCartDao = shoppingCartDao;
+        this.userDao = userDao;
+        this.productDao = productDao;
+    }
 
     // each method in this controller requires a Principal object as a parameter
     public ShoppingCart getCart(Principal principal)
@@ -41,16 +48,28 @@ public class ShoppingCartController
         }
     }
 
-    // add a POST method to add a product to the cart - the url should be
+    //TODO add a POST method to add a product to the cart - the url should be
     // https://localhost:8080/cart/products/15 (15 is the productId to be added
 
+    public ShoppingCart addToShoppingCart(@PathVariable int productId){
+        return null;
+    }
 
-    // add a PUT method to update an existing product in the cart - the url should be
-    // https://localhost:8080/cart/products/15 (15 is the productId to be updated)
-    // the BODY should be a ShoppingCartItem - quantity is the only value that will be updated
+
+    // TODO - add a PUT method to update an existing product in the cart - the url should be
+    //  https://localhost:8080/cart/products/15 (15 is the productId to be updated)
+    //  the BODY should be a ShoppingCartItem - quantity is the only value that will be updated
+
+    public ShoppingCart updateShoppingCart() {
+        return null;
+    }
 
 
-    // add a DELETE method to clear all products from the current users cart
-    // https://localhost:8080/cart
+    // TODO - add a DELETE method to clear all products from the current users cart
+    //  https://localhost:8080/cart
+
+    public ShoppingCart removeFromCart() {
+        return null;
+    }
 
 }
